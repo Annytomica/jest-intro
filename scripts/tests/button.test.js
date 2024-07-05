8da4d82e47adaa19db85f762df4c97539580cccb
Anny Devoy <annydevoyalt@gmail.com>
2024-07-05 14:37:11 +0000

    Add button test with index.html loaded in jsdom

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,14 +5,21 @@
 const { TestScheduler } = require("jest");
 const buttonClick = require("../button");
 
-// loads the mock DOM with the mock features required for test before any tests run
+// loads the mock DOM with the mock features required for test before any tests run. Use as boilerplate.
 beforeAll(() => {
-    document.body.innerHTML = '<p id="par" ></p>';
+    // document.body.innerHTML = '<p id="par" ></p>'; //do this if just testing a small part of a web page
+    let fs = require('fs'); //load the 'fs' file system handling module that is built into node.js
+    let fileContents = fs.readFileSync('index.html', 'utf-8'); //saves html to variable using utf-8 character set
+    //attaching the html to the mock DOM. 
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
-describe('DOM tests', () => {
-    test('expects p content to change', () => {
-        buttonClick(); // simulates button being clicked as needed for test
-        expect(document.getElementById("par").innerHTML).toEqual('You Clicked');
-    })
+describe("DOM tests", () => {
+    test("Expects content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par")
+            .innerHTML).toEqual("You Clicked");
+    });
 })
